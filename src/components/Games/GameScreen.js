491cfd77fe423ceba09a585aa7f837c183164671
Angelos Moavinis <amoavinis@car.gr>
2026-09-04 09:07:15 +0300

    game 18

--- src/components/Games/GameScreen.js
+++ src/components/Games/GameScreen.js
@@ -22,6 +22,7 @@ import Game14 from "./Game14";
 import Game15 from "./Game15";
 import Game16 from "./Game16";
 import Game17 from "./Game17";
+import Game18 from "./Game18";
 
 // Every playable exercise, keyed by its game id. Set B (17-31) is added here as
 // each component lands; the set a user may reach is decided by games.js.
@@ -43,6 +44,7 @@ const gameComponents = {
   15: Game15,
   16: Game16,
   17: Game17,
+  18: Game18,
 };
 
 const GameScreen = () => {
